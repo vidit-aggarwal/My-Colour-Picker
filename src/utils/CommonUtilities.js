d388054da252fe942edcde36e88f9044074f97ba
ArvindSinghRawat <arvind160102261@gmail.com>
2020-10-19 18:42:33 +0530

    Added Utility package
    - Added Common utility to convert HexCode to RGB values.

---
+++ src/utils/CommonUtilities.js
@@ -0,0 +1,10 @@
+export const hexToRgb = (hexCode) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexCode);
+  return result
+    ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16),
+      ]
+    : null;
+};
