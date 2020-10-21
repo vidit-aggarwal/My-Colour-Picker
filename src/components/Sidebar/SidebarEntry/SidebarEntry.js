a542368f9297195653ee960a5b17511282b468f2
ArvindSinghRawat <arvind160102261@gmail.com>
2020-10-22 00:44:04 +0530

    Added Sidebar Entry for Sidebars

---
+++ src/components/Sidebar/SidebarEntry/SidebarEntry.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+const SidebarEntry = (props) => {
+  const Wrapper = styled.div`
+    width: 100%;
+    height: 100px;
+    color: lightblue;
+    display: inline-flex;
+    justify-content: center;
+    vertical-align: middle;
+    background-color: ${props.backgroundColour};
+    font-weight: bold;
+    font-size: 2.5rem;
+    font-outline: black;
+    transition: all 0.5s ease-out;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+
+    &:hover {
+      backdrop-filter: blur(10px);
+      background: url(${props.backgroundImage});
+      background-color: ${props.backgroundColour};
+      background-size: cover;
+      background-position: center;
+      transition: all 0.5s ease-out;
+    }
+  `;
+
+  const Content = styled.div`
+    align-self: center;
+  `;
+
+  return (
+    <Wrapper>
+      <Content>{props.name}</Content>
+    </Wrapper>
+  );
+};
+
+export default SidebarEntry;
