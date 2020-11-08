66b76bbc9a93be0db2a8e59ee3565cc9cfeffe82
ArvindSinghRawat <arvind160102261@gmail.com>
2020-11-09 01:51:15 +0530

    Moved Styled Components out of Render Method

--- src/components/Sidebar/SidebarEntry/SidebarEntry.js
+++ src/components/Sidebar/SidebarEntry/SidebarEntry.js
@@ -3,23 +3,30 @@ import styled from "styled-components";
 
 import styles from "./SidebarEntry.module.css";
 
+const Wrapper = styled.div`
+  width: ${(props) => (props.active ? "99%" : "100%")};
+  height: ${(props) => (props.active ? "96px" : "100px")};
+  background-color: ${(props) => props.backgroundColour};
+  border: ${(props) => (props.active ? "2px groove " + props.inverse : "none")};
+
+  &:hover {
+    background: url(${(props) => props.backgroundImage});
+    background-color: ${(props) => props.backgroundColour};
+    background-size: cover;
+    background-position: center;
+  }
+`;
+
 const SidebarEntry = (props) => {
-  const Wrapper = styled.div`
-    width: ${props.active ? "99%" : "100%"};
-    height: ${props.active ? "96px" : "100px"};
-    background-color: ${props.backgroundColour};
-    border: ${props.active ? "2px groove " + props.inverse : "none"};
-
-    &:hover {
-      background: url(${props.backgroundImage});
-      background-color: ${props.backgroundColour};
-      background-size: cover;
-      background-position: center;
-    }
-  `;
-
   return (
-    <Wrapper className={styles.SidebarEntry} onClick={props.changeProfile}>
+    <Wrapper
+      className={styles.SidebarEntry}
+      active={props.active}
+      backgroundColour={props.backgroundColour}
+      backgroundImage={props.backgroundImage}
+      inverse={props.inverse}
+      onClick={props.changeProfile}
+    >
       <div className={styles.Content}>{props.name}</div>
     </Wrapper>
   );
