75bd55a94601c33bf6c836412079c69d018fad01
ArvindSinghRawat <arvind160102261@gmail.com>
2020-11-17 01:41:16 +0530

    Improved Hamburger Background

--- src/components/Sidebar/Hamburger/HamburgerWrapper.js
+++ src/components/Sidebar/Hamburger/HamburgerWrapper.js
@@ -4,13 +4,26 @@ import Hamburger from "./Hamburger";
 import style from "./HamburgerWrapper.module.css";
 
 const Content = styled.div`
-  background: ${({ open }) => (open ? "#EFFFFA" : "transparent")};
-  box-shadow: ${({ open }) => (open ? "0px 0 4px 4px white" : "")};
+  ${(props) => (props.open ? props.background : "")}
 `;
+
+const background = `  background: rgb(255, 255, 255);
+background: radial-gradient(
+  circle at top left,
+  rgba(255, 255, 255, 1) 0%,
+  rgba(255, 255, 255, 0.8) 33%,
+  rgba(255, 255, 255, 0.6) 66%,
+  rgba(255, 255, 255, 0.3) 75%,
+  rgba(255, 255, 255, 0) 100%
+);`;
 
 const HamburgerWrapper = ({ open, setOpen }) => {
   return (
-    <Content open={open} className={style.HamburgerWrapper}>
+    <Content
+      open={open}
+      className={style.HamburgerWrapper}
+      background={background}
+    >
       <Hamburger open={open} setOpen={setOpen}></Hamburger>
     </Content>
   );
